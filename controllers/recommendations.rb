984971530ce14261b75e97626f8abbf7d05395e0
kevin deutscher <kevin.m.deutscher@gmail.com>
2015-12-01 16:11:02 -0600

    almost working

--- controllers/recommendations.rb
+++ controllers/recommendations.rb
@@ -3,13 +3,34 @@ class RecommendationsController < ApplicationController #will have access to eve
 
   get '/' do
     # 'works'
+    @recommendation = Recommendation.all
+    erb :read_recommendation
+  end
+
+  get '/create' do
+    erb :create_recommendation
+  end
+
+
+  post '/create/'do
+    p params
+    @rec = Recommendation.new
+    @rec.contributor = params[:contributor]
+    @rec.place = params[:name]
+    @rec.typeOfPlace = params[:type_of_place]
+    @rec.notes = params[:notes]
+    @rec.save
     erb :index
   end
+
 
   get '/api' do
     # same as select * from recommendations
     Recommendation.all.to_json
   end
 
-
+  get '/api/:id' do
+    p params
+    return Recommendation.find(params[:id]).to_json
+  end
 end
